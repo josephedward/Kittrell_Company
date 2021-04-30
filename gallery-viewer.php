bbcb87438860176f3ddca7b49ffd615899f3cf69
JoeV <josephedwardwork@gmail.com>
2021-04-29 22:04:01 -0400

    organized templates, fixed gallery images

--- gallery-viewer.php
+++ gallery-viewer.php
@@ -1,13 +1,22 @@
-
 <div class="container project-view">
-    
-	<div class="row">
-        <div class="col-md-12 project-images"><?php
-if(isset($_GET['item'])):
-	$item = $_GET['item'];
-?>
-            <img src="images/gallery/<?php echo $item; ?>.jpg" alt="" class="img-responsive" />
-<?php endif; ?>
+
+        <div class="row">
+                <div class="col-md-12 project-images gallery-image">
+
+                        <?php
+                        if (isset($_GET['item'])) :
+                                $item = $_GET['item'];
+                        ?>
+                                <script>
+                                        console.log(imageSource)
+                                        // `images/gallery/${imageSource}.jpg`
+                                        imageString = `<img src="images/gallery/${imageSource}.jpg" alt="" class="img-responsive" />`
+                                        localDom = $("div.container.project-view")
+                                        localDom.append(imageString)
+                                </script>
+
+
+                        <?php endif; ?>
+                </div>
         </div>
-    </div>
 </div>
